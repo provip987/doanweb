09cdaa3832e599442ae955dbe9b2bfbc0e8836a5
provip987 <dotrongkhiemvippro@gmail.com>
2023-11-21 15:07:12 +0700

    code lavarel

---
+++ database/migrations/2023_11_21_065319_create_admins_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id();
+            $table->string('ten');
+            $table->string('ten_dang_nhap');
+            $table->string('password');
+            $table->string('email');
+            $table->integer('quyen');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin');
+    }
+};
